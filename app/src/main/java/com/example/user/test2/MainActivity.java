275cca4379972bfb4325971600154b637ba3f9a6
Jisu <cwyh5526@gmail.com>
2016-07-07 17:31:45 +0900

    YES!!!! Camera preview test done >< feel goooooooooooood!!!

--- app/src/main/java/com/example/user/test2/MainActivity.java
+++ app/src/main/java/com/example/user/test2/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.user.test2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
+
+    public static final String TAG ="Main Activity";
+    private static final int REQUEST_CAMERA=0;
+    private boolean mLogShown;
+    private View mLayout;
+
+
+    public void showCamera (View view){
+        Log.i(TAG,"Show camera button pressed. Checking permission.");
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            requestCameraPermission();
+        }else{
+            Log.i(TAG,"CAMERA permission has already been granted. Displaying camera preview.");
+            showCameraPreview();
+        }
+
+    }
+
+    private void requestCameraPermission(){
+        Log.i(TAG,"CAMERA permission has NOT been granted. Requesting permission.");
+
+
+        // Camera permission has not been granted yet. Request it directly.
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                REQUEST_CAMERA);
+
+
+    }
+
+    private void showCameraPreview() {
+        CameraPreviewFragment camera_preview_fragment = new CameraPreviewFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.camera_fragment, camera_preview_fragment)
+                .addToBackStack("contacts")
+                .commit();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLayout = findViewById(R.id.main_layout);
+
+        if(savedInstanceState==null){
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            RuntimePermissionsFragment fragment = new RuntimePermissionsFragment();
+            transaction.replace(R.id.camera_fragment, fragment);
+            transaction.commit();
+        }
+
     }
+    public void onPause(){
+        super.onPause();
+    }
+
+    public  void onDestroy()
+    {
+        super.onDestroy();
+    }
+
 }
---
+++ app/src/main/java/com/example/user/test2/RuntimePermissionsFragment.java
@@ -0,0 +1,23 @@
+package com.example.user.test2;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 2016-07-07.
+ */
+public class RuntimePermissionsFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, null);
+
+        return root;
+    }
+}
